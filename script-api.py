ab8498c78e3a64e4ef491b52f4da64b58a3ab113
Gregory Pacheco <gregory.pacheco@flipdish.com>
2021-10-27 23:11:46 +0100

    slide palestra

--- script-api.py
+++ script-api.py
@@ -2,10 +2,12 @@ import requests
 
 key = '810f7ab17a074a5bbb6ce92f9ea3eb7a'
 units = 'metric'
-cidade = 'buritizeiro'
+cidade = 'pirapora'
 url = f'https://api.openweathermap.org/data/2.5/weather?q={cidade}&units={units}&appid={key}'
 
 response = requests.get(url)
 
 temp = response.json()['main']['temp']
+cidade = response.json()['name']
+print(cidade)
 print(temp)
